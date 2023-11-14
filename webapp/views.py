922233bdf42f2a92d11b0386e15445f4bb2e1a5e
odalys quiñonez <135558827+Odalysquinonez@users.noreply.github.com>
2023-11-14 14:34:59 -0500

    Add files via upload

---
+++ webapp/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from personas.models import Persona
+
+
+# Create your views here.
+def Bienvenido (request):
+    #mensajes ={'msg1': 'valor mensaje 1', 'msg2': 'valor mensaje 2'}
+    no_personas = Persona.objects.count()
+    personas = Persona.objects.all()
+    #return render(request,"bienvenido.html",{'msg1': 'valor mensaje 1', 'msg2': 'valor mensaje 2'} )
+    return render(request,"bienvenido.html",{'no_personas':no_personas,'personas': personas})
+
+
+
+
+
+
+
+def Contacto (request):
+    return HttpResponse("Mi nombre es Odalys y mi telefono es 0967605159")
+
+def Chao (request):
+    return HttpResponse("Chao Mundo")
